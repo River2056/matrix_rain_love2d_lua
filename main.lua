28d5d8e2a4384397ac831707f8638ba0a7fde7c3
river2056 <chen0625tung@gmail.com>
2022-07-31 22:30:10 +0800

    + expand carpool, added more characters for variety + refactor local function generateChar into Stream method + added deltatime field in Stream for different falling speed + added constants

--- main.lua
+++ main.lua
@@ -2,181 +2,36 @@
 --    self attempt on the matrix rain effect
 -- ]]
 local font, fontSize, dtTotal, streams
+local streamDeltaTime = 100
+local updateDeltaTime = 1200
 local charpool = {
-    "ァ",
-    "ア",
-    "ィ",
-    "イ",
-    "ゥ",
-    "ウ",
-    "ェ",
-    "エ",
-    "ォ",
-    "オ",
-    "カ",
-    "ガ",
-    "キ",
-    "ギ",
-    "ク",
-    "グ",
-    "ケ",
-    "ゲ",
-    "コ",
-    "ゴ",
-    "サ",
-    "ザ",
-    "シ",
-    "ジ",
-    "ス",
-    "ズ",
-    "セ",
-    "ゼ",
-    "ソ",
-    "ゾ",
-    "タ",
-    "ダ",
-    "チ",
-    "ヂ",
-    "ッ",
-    "ツ",
-    "ヅ",
-    "テ",
-    "デ",
-    "ト",
-    "ド",
-    "ナ",
-    "ニ",
-    "ヌ",
-    "ネ",
-    "ノ",
-    "ハ",
-    "バ",
-    "パ",
-    "ヒ",
-    "ビ",
-    "ピ",
-    "フ",
-    "ブ",
-    "プ",
-    "ヘ",
-    "ベ",
-    "ペ",
-    "ホ",
-    "ボ",
-    "ポ",
-    "マ",
-    "ミ",
-    "ム",
-    "メ",
-    "モ",
-    "ャ",
-    "ヤ",
-    "ュ",
-    "ユ",
-    "ョ",
-    "ヨ",
-    "ラ",
-    "リ",
-    "ル",
-    "レ",
-    "ロ",
-    "ヮ",
-    "ワ",
-    "ヰ",
-    "ヱ",
-    "ヲ",
-    "ン",
-    "ヴ",
-    "ヵ",
-    "ヶ",
-    "ヷ",
-    "ヸ",
-    "ヹ",
-    "ヺ",
-    "ぁ",
-    "あ",
-    "ぃ",
-    "い",
-    "ぅ",
-    "う",
-    "ぇ",
-    "え",
-    "ぉ",
-    "お",
-    "か",
-    "が",
-    "き",
-    "ぎ",
-    "く",
-    "ぐ",
-    "け",
-    "げ",
-    "こ",
-    "ご",
-    "さ",
-    "ざ",
-    "し",
-    "じ",
-    "す",
-    "ず",
-    "せ",
-    "ぜ",
-    "そ",
-    "ぞ",
-    "た",
-    "だ",
-    "ち",
-    "ぢ",
-    "っ",
-    "つ",
-    "づ",
-    "て",
-    "で",
-    "と",
-    "ど",
-    "な",
-    "に",
-    "ぬ",
-    "ね",
-    "の",
-    "は",
-    "ば",
-    "ぱ",
-    "ひ",
-    "び",
-    "ぴ",
-    "ふ",
-    "ぶ",
-    "ぷ",
-    "へ",
-    "べ",
-    "ぺ",
-    "ほ",
-    "ぼ",
-    "ぽ",
-    "ま",
-    "み",
-    "む",
-    "め",
-    "も",
-    "ゃ",
-    "や",
-    "ゅ",
-    "ゆ",
-    "ょ",
-    "よ",
-    "ら",
-    "り",
-    "る",
-    "れ",
-    "ろ",
-    "ゎ",
-    "わ",
-    "ゐ",
-    "ゑ",
-    "を",
-    "ん",
-    "ゔ",
+    "ァ", "ア", "ィ", "イ", "ゥ", "ウ", "ェ", "エ", "ォ", "オ", "カ", "ガ", "キ", "ギ", "ク", "グ", "ケ",
+    "ゲ", "コ", "ゴ",
+    "サ", "ザ", "シ", "ジ", "ス", "ズ", "セ", "ゼ", "ソ", "ゾ", "タ", "ダ", "チ", "ヂ", "ッ", "ツ", "ヅ",
+    "テ", "デ", "ト",
+    "ド", "ナ", "ニ", "ヌ", "ネ", "ノ", "ハ", "バ", "パ", "ヒ", "ビ", "ピ", "フ", "ブ", "プ", "ヘ", "ベ",
+    "ペ", "ホ", "ボ",
+    "ポ", "マ", "ミ", "ム", "メ", "モ", "ャ", "ヤ", "ュ", "ユ", "ョ", "ヨ", "ラ", "リ", "ル", "レ", "ロ",
+    "ヮ", "ワ", "ヰ",
+    "ヱ", "ヲ", "ン", "ヴ", "ヵ", "ヶ", "ヷ", "ヸ", "ヹ", "ヺ",
+    "ぁ", "あ", "ぃ", "い", "ぅ", "う", "ぇ", "え", "ぉ", "お", "か", "が", "き", "ぎ", "く", "ぐ", "け",
+    "げ", "こ", "ご",
+    "さ", "ざ", "し", "じ", "す", "ず", "せ", "ぜ", "そ", "ぞ", "た", "だ", "ち", "ぢ", "っ", "つ", "づ",
+    "て", "で", "と",
+    "ど", "な", "に", "ぬ", "ね", "の", "は", "ば", "ぱ", "ひ", "び", "ぴ", "ふ", "ぶ", "ぷ", "へ", "べ",
+    "ぺ", "ほ", "ぼ",
+    "ぽ", "ま", "み", "む", "め", "も", "ゃ", "や", "ゅ", "ゆ", "ょ", "よ", "ら", "り", "る", "れ", "ろ",
+    "ゎ", "わ", "ゐ",
+    "ゑ", "を", "ん", "ゔ",
+    "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W",
+    "X",
+    "Y", "Z",
+    "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w",
+    "x",
+    "y", "z",
+    "0", "1", "2", "3", "4", "5", "6", "7", "8", "9",
+    "!", "\"", "#", "$", "%", "&", "\'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?",
+    "@", "[", "\\", "]", "^", "_", "`", "{", "|", "}", "~"
 }
 
 function Symbol(x, y, text)
@@ -193,9 +48,23 @@ end
 
 function Stream()
     local st = {}
+    st.initialX = 0
     st.symbols = {} -- array of symbols
     st.lengthCount = math.random(15, 70)
     st.count = math.random(-1000, 1)
+    st.deltatime = 0
+
+    st.generateChar = function(self)
+        local symbol = Symbol(self.initialX, (self.count * fontSize) % HEIGHT, charpool[math.random(1, #charpool)])
+        table.insert(self.symbols, 1, symbol)
+        self.count = self.count <= HEIGHT and self.count + 1 or 0
+        if self.count == 0 then
+            self.lengthCount = math.random(15, 70)
+        end
+        if #self.symbols >= self.lengthCount then
+            table.remove(self.symbols, #self.symbols - 0)
+        end
+    end
 
     return st
 end
@@ -204,7 +73,7 @@ function love.load()
     -- fullscreen mode
     math.randomseed(os.time())
     love.window.setFullscreen(true)
-    WIDTH = love.graphics.getWidth() 
+    WIDTH = love.graphics.getWidth()
     HEIGHT = love.graphics.getHeight()
     fontSize = 10
     font = love.graphics.newFont("fonts/NotoSansJP-Regular.otf", fontSize)
@@ -212,6 +81,8 @@ function love.load()
     streams = {}
     for x = 1, WIDTH, fontSize do
         local s = Stream()
+        s.initialX = x
+        s.deltatime = math.random() / streamDeltaTime
         local symbol = Symbol(x, s.count, charpool[math.random(1, #charpool)])
         table.insert(s.symbols, 1, symbol)
         s.count = s.count + 1
@@ -225,26 +96,15 @@ function love.keypressed(key)
     end
 end
 
-local function generateChar(x, stream)
-    local symbol = Symbol(x, (stream.count * fontSize) % HEIGHT, charpool[math.random(1, #charpool)])
-    table.insert(stream.symbols, 1, symbol)
-    stream.count = stream.count <= HEIGHT and stream.count + 1 or 0
-    if stream.count == 0 then
-        stream.lengthCount = math.random(15, 70)
-    end
-    if #stream.symbols >= stream.lengthCount then
-        table.remove(stream.symbols, #stream.symbols - 0)
-    end
-end
-
 function love.update(dt)
     dtTotal = dtTotal + dt
-    if dtTotal >= 0.07 then
-        dtTotal = 0
-        for _, stream in ipairs(streams) do
-            local x = #stream.symbols > 0 and stream.symbols[1].x or 0
-
-            generateChar(x, stream)
+    local deter = math.random() / updateDeltaTime
+    for _, stream in ipairs(streams) do
+        if dtTotal % stream.deltatime <= deter then
+            stream:generateChar()
+            if stream.symbols[1].y < stream.symbols[#stream.symbols].y then
+                stream.deltatime = math.random() / streamDeltaTime
+            end
         end
     end
 end
@@ -253,10 +113,11 @@ function love.draw()
     love.graphics.setBackgroundColor(0, 0, 0)
     for _, stream in ipairs(streams) do
         for idx, sym in ipairs(stream.symbols) do
+            local c = 1 / #stream.symbols
             if idx == 1 then
                 love.graphics.setColor(0.68, 0.78, 0.69)
             else
-                love.graphics.setColor(0.40, 0.73, 0.40)
+                love.graphics.setColor(0.40, 0.73, 0.40, 1 - c * idx)
             end
             sym.render()
         end
